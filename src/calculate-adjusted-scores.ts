91cee26ac4dce8d44982a0f233bfad490085b846
Todd Zusman <toddzusman@gmail.com>
2020-09-27 23:14:50 -0600

    Remove compiled scss

--- src/calculate-adjusted-scores.ts
+++ src/calculate-adjusted-scores.ts
@@ -1,18 +1,17 @@
 type concepts = (string | number)[][]
 
 export default (concepts: concepts, weights: Array<number>) => {
-  const maxWeight = Math.max(...weights)
-
-  const a = concepts.reduce<concepts>((memo, set, idx) => {
+  const normalized = concepts.reduce<concepts>((memo, set, idx) => {
     const [concept, ...scores] = set
     const adjustedValues = scores.map((score, i) => ((score as number) * weights[i]) / 100.0)
     memo.push([concept, ...adjustedValues])
     return memo
   }, [])
 
-  const adjustedScores = a.map(set => {
+  const maxWeight = Math.max(...weights)
+  const adjustedScores = normalized.map(set => {
     const [concept, ...scores] = set
-    const adjusted = (scores as number[]).map((score, idx) => score * (100 / maxWeight))
+    const adjusted = (scores as number[]).map(score => score * (100 / maxWeight))
     return [concept, ...adjusted]
   })
 
